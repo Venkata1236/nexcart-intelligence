d757a3e58503dee47f1f22c8d7db99c79da40703
Venkata Reddy <bommavaramvenkat2003@gmail.com>
2026-04-25 19:40:53 +0530

    feat: add Spinner component

---
+++ frontend/src/components/ui/Spinner.jsx
@@ -0,0 +1,10 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-8 h-8 border-4 border-indigo-200 border-t-indigo-600
+        rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default Spinner;
